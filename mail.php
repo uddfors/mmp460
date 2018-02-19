69ef60b522a86c351da436e99937c42d8b6edbae
uddfors <36239674+uddfors@users.noreply.github.com>
2018-02-19 18:52:25 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,11 +4,11 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="Contact form\n\nFrom: $name \nMessage: $message";
-$recipient = "hello@uddfors.com";
+$recipient = "charlotteuddfors@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You! E-mail send.";
+echo "Thank You! E-mail sent.";
 
 ?>
